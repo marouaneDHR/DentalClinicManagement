eafd9a1c2559b66ebbe872acedf8f6994b3ba0e2
Marouane DH <157962704+marouaneDHR@users.noreply.github.com>
2025-01-19 02:32:37 +0100

    Delete intellij_Projects/JEEProject directory

--- intellij_Projects/JEEProject/BackEndProject/src/main/java/com/example/demo14/Repository/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo14.Repository;
-
-import com.example.demo14.Entities.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-}
